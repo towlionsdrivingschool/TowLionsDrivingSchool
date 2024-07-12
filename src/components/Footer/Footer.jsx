dee0d0f6cd9220a977a3adadf9a8a70bf5c72b50
towlionsdrivingschool <towlionsdrivingschool@gmail.com>
2024-07-12 19:39:01 +0530

    footer contact updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -29,16 +29,16 @@ const Footer = () => {
   return (
     <div className="bg-gray-100 dark:bg-dark mt-14 rounded-t-3xl">
       <section className="container">
-        <div className=" grid md:grid-cols-3 py-5">
+        <div className=" grid md:grid-cols-3 py-">
           {/* company Details */}
-          <div className=" py-8 px-4 ">
-            <h1 className="sm:text-3xl text-xl font-bold sm:text-left text-justify mb-3 flex items-center gap-3 font-serif">
-              Car Rental
+          <div className=" py-8 px-4">
+            <h1 className="sm:text-2xl text-xl font-bold sm:text-left text-justify mb-3 flex items-center gap-3 font-serif">
+            Two Lions Driving School
             </h1>
-            <p className="text-sm">
+            {/* <p className="text-sm">
               Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet
               consectetur adipisicing elit. Possimus, voluptate.{" "}
-            </p>
+            </p> */}
             <br />
             <div className="flex items-center gap-3">
               <FaLocationArrow />
@@ -51,18 +51,20 @@ const Footer = () => {
             {/* Social Handle */}
             <div className="flex items-center gap-3 mt-6">
               <a href="#">
-                <FaInstagram className="text-3xl hover:text-primary duration-300" />
+                <FaInstagram className="text-2xl hover:text-primary duration-300" />
               </a>
               <a href="#">
-                <FaFacebook className="text-3xl hover:text-primary duration-300" />
+                <FaFacebook className="text-2xl hover:text-primary duration-300" />
               </a>
               <a href="#">
-                <FaLinkedin className="text-3xl hover:text-primary duration-300" />
+                <FaLinkedin className="text-2xl hover:text-primary duration-300" />
               </a>
             </div>
           </div>
           {/* Links */}
           <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10 ">
+          <div className=""></div>
+          <div className=""></div>
             <div className="">
               <div className="py-8 px-4 ">
                 <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
@@ -78,37 +80,8 @@ const Footer = () => {
                 </ul>
               </div>
             </div>
-            <div className="">
-              <div className="py-8 px-4 ">
-                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
-                  Links
-                </h1>
-                <ul className="flex flex-col gap-3">
-                  {FooterLinks.map((link) => (
-                    <li className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-500 dark:text-gray-200">
-                      <span>&#11162;</span>
-                      <span>{link.title}</span>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-            <div className="">
-              <div className="py-8 px-4 ">
-                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
-                  Location
-                </h1>
-                {/* <ul className="list-disc list-inside"> */}
-                <ul className="flex flex-col gap-3">
-                  {FooterLinks.map((link) => (
-                    <li className="cursor-pointer hover:translate-x-1 duration-300 hover:!text-primary space-x-1 text-gray-500 dark:text-gray-200">
-                      <span>&#11162;</span>
-                      <span>{link.title}</span>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
+            
+            
           </div>
         </div>
         <span class="block  text-gray-500 sm:text-center dark:text-gray-400">
